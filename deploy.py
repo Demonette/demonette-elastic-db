98794e24f43f0692f65c6674b6af7299c0eb3463
Simon Meoni <simonmeoni@aol.com>
2018-03-21 15:26:29 +0100

    add mapping for es databate

--- deploy.py
+++ deploy.py
@@ -4,15 +4,26 @@ es = Elasticsearch()
 es.indices.delete(index='demonette', ignore=[400, 404])
 es.indices.create(index='demonette')
 header = open('header.csv', 'r').readlines()[0].split('\t')
+map = open('fields-to-mapped.txt', 'r').readlines()
 db_csv = open('demonette-1.2.csv', 'r', encoding='ISO-8859-3').readlines()
 cpt = 0
-for l in db_csv:
+
+for m in map:
+    d_map = {
+        "properties": {
+            m.replace('\n', ''): {
+                "type": "text",
+                "fielddata": "true"
+            }
+        }
+    }
+    es.indices.put_mapping(index='demonette', doc_type='relation', body=d_map)
+
+for l in db_csv[1:10]:
     if l:
         d_dict = {}
         l_l = l.split(',')
         for idx, el in enumerate(header):
             d_dict[el] = l_l[idx].replace('\"', '').replace('\n', '')
-
         es.create(index='demonette', doc_type='relation', id=cpt, body=d_dict)
         cpt += 1
-        print('write :' + str(d_dict))
