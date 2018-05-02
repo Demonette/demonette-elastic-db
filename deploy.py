47b51925d9aa814ab7e9a986237f553da3ff3924
Simon Meoni <simonmeoni@aol.com>
2018-05-02 15:17:40 +0200

    add a custom tokenizer to escape special char '@' this close #3

--- deploy.py
+++ deploy.py
@@ -1,4 +1,5 @@
 from elasticsearch import Elasticsearch
+import re
 es = Elasticsearch()
 
 es.indices.delete(index='demonette', ignore=[400, 404])
@@ -6,17 +7,47 @@ es.indices.create(index='demonette')
 header = open('header.csv', 'r').readlines()[0].split('\t')
 map = open('fields-to-mapped.txt', 'r').readlines()
 db_csv = open('demonette-1.2.csv', 'r', encoding='ISO-8859-3').readlines()
+
+analyzer = {
+    "analysis": {
+      "analyzer": {
+        "custom_analyzer": {
+          "type": "custom",
+          "tokenizer": "whitespace",
+          "filter": [
+            "lowercase",
+          ]
+        }
+      }
+    }
+}
+es.indices.close(index='demonette')
+es.indices.put_settings(index='demonette', body=analyzer)
+es.indices.open(index='demonette')
+
 cpt = 0
 
 for m in map:
-    d_map = {
-        "properties": {
-            m.replace('\n', ''): {
-                "type": "text",
-                "fielddata": "true"
+    if re.match('type_[0-9]', m):
+        d_map = {
+            "properties": {
+                m.replace('\n', ''): {
+                    "type": "text",
+                    "fielddata": "true",
+                    "analyzer": "custom_analyzer"
+                }
             }
         }
-    }
+    else:
+        d_map = {
+                "properties": {
+                    m.replace('\n', ''): {
+                        "type": "text",
+                        "fielddata": "true",
+                        "analyzer": "custom_analyzer"
+                }
+            }
+        }
     es.indices.put_mapping(index='demonette', doc_type='relation', body=d_map)
 
 for l in db_csv:
